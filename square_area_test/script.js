d5e557aa118b5bf7bc1d24bb1e949023b614c999
choitjddn0311 <cswcjw311427@naver.com>
2024-07-01 09:48:04 +0900

    feat : add square area calculate

---
+++ square_area_test/script.js
@@ -0,0 +1,15 @@
+// function cal(){
+//     // let g = document.ff.g.value;  일반적으로 form 안에 g라는 input에서 value를 가져옴
+//     // let s = document.ff.s.value;
+//     let g = document.getElementById('g').value; //id를 활용하여 input값의 value를 가져옴
+//     let s = document.getElementById('s').value;
+//     let c = g*s;
+//     alert("직사각형의 넓이는" + c + "입니다.");  
+// }
+// // 버튼 클릭 함수
+
+function cal() {
+    let g = document.getElementById('g').value;
+    let s = document.getElementById('s').value;
+    let ss = document.ff.c.value = g*s;
+}
